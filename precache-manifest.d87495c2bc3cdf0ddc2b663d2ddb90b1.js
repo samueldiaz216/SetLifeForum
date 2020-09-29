25a6f82c198227ce2640219786290884b2b5fdda
samueldiaz216 <samueldiaz216@gmail.com>
2020-09-29 19:28:46 -0400

    Updates

---
+++ precache-manifest.d87495c2bc3cdf0ddc2b663d2ddb90b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7ab9336c3991a3974e09c2c56a21abb",
+    "url": "/SetLifeForum/index.html"
+  },
+  {
+    "revision": "46c43d2e844071b7f8e7",
+    "url": "/SetLifeForum/static/js/2.a5d22535.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SetLifeForum/static/js/2.a5d22535.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a906aba246f86b6f27eb",
+    "url": "/SetLifeForum/static/js/main.7b8d1564.chunk.js"
+  },
+  {
+    "revision": "e047d57cf97b4e014a23",
+    "url": "/SetLifeForum/static/js/runtime-main.e3c3dbf0.js"
+  }
+]);
